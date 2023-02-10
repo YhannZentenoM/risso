14075960bb29054b423e1c12461c3e09397316c5
CCIA <yzenteno@cciarequipa.org>
2023-02-09 19:45:48 -0500

    web finish 95%

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // site: 'https://polaco300.github.io',
-  // base: '/risso'
+  site: 'https://polaco300.github.io',
+  base: '/risso'
 });
